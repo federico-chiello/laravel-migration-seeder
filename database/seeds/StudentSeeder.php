8f4ec0edcc16e1c3918035ae932a93c6e85f4105
Fede <chiellofederico@gmail.com>
2021-03-30 21:31:51 +0200

    utilizzo i faker per creare i dati della tabella

--- database/seeds/StudentSeeder.php
+++ database/seeds/StudentSeeder.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use App\Student;
-use Carbon\Carbon;
+use Faker\Generator as Faker;
+//Carbon serve nel primo metodo per la data.
+// use Carbon\Carbon;
 
 class StudentSeeder extends Seeder
 {
@@ -11,17 +13,28 @@ class StudentSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $students = config('students');
+        //Primo metodo: creo un array a mano con i dati che voglio.
+        // $students = config('students');
 
-        foreach($students as $student){
+        // foreach($students as $student){
+        //     $newStudent = new Student();
+
+        //     $newStudent->nome = $student['nome'];
+        //     $newStudent->citta = $student['citta'];
+        //     $newStudent->data_nascita = Carbon::createFromFormat('d/m/Y', $student['data_nascita']);
+
+        //     $newStudent->save();
+        // }
+
+
+        //Secondo metodo: utilizzo i dati faker.
+        for ($i = 0; $i < 10; $i++){
             $newStudent = new Student();
-
-            $newStudent->nome = $student['nome'];
-            $newStudent->citta = $student['citta'];
-            $newStudent->data_nascita = Carbon::createFromFormat('d/m/Y', $student['data_nascita']);
-
+            $newStudent->nome = $faker->name();
+            $newStudent->citta = $faker->city();
+            $newStudent->data_nascita = $faker->date();
             $newStudent->save();
         }
     }
