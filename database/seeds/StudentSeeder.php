23b766f83b13bef72f286585eb2cab8d75485431
Fede <chiellofederico@gmail.com>
2021-03-30 21:05:58 +0200

    utilizzo il db:seed per vederlo nel db

--- database/seeds/StudentSeeder.php
+++ database/seeds/StudentSeeder.php
@@ -17,17 +17,17 @@ class StudentSeeder extends Seeder
             [
                 'nome' => 'Paolo',
                 'citta' => 'Roma',
-                'data_nascita' => '02/05/98'
+                'data_nascita' => '02/05/1998'
             ],
             [
                 'nome' => 'Giacomo',
                 'citta' => 'Milano',
-                'data_nascita' => '13/07/87'
+                'data_nascita' => '13/07/1987'
             ],
             [
                 'nome' => 'Aldo',
                 'citta' => 'Palermo',
-                'data_nascita' => '27/11/95'
+                'data_nascita' => '27/11/1995'
             ]
         ];
 
