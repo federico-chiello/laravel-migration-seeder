66f34c37d81173fe36c806e35359baa8878a8372
Fede <chiellofederico@gmail.com>
2021-03-30 20:39:22 +0200

    creo un array di studenti

--- database/seeds/StudentSeeder.php
+++ database/seeds/StudentSeeder.php
@@ -12,6 +12,22 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $students = [
+            [
+                'nome' => 'Paolo',
+                'citta' => 'Roma',
+                'data_nascita' => '02/05/98'
+            ],
+            [
+                'nome' => 'Giacomo',
+                'citta' => 'Milano',
+                'data_nascita' => '13/07/87'
+            ],
+            [
+                'nome' => 'Aldo',
+                'citta' => 'Palermo',
+                'data_nascita' => '27/11/95'
+            ]
+        ];
     }
 }
