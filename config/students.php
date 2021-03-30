2b67958476e8a5c4ffd7edcd4b0967bd1367d661
Fede <chiellofederico@gmail.com>
2021-03-30 21:17:53 +0200

    l'array degli studenti lo metto dentro config e lo richiamo nel Seeder

---
+++ config/students.php
@@ -0,0 +1,21 @@
+<?php
+
+
+return
+[
+    [
+        'nome' => 'Paolo',
+        'citta' => 'Roma',
+        'data_nascita' => '02/05/1998'
+    ],
+    [
+        'nome' => 'Giacomo',
+        'citta' => 'Milano',
+        'data_nascita' => '13/07/1987'
+    ],
+    [
+        'nome' => 'Aldo',
+        'citta' => 'Palermo',
+        'data_nascita' => '27/11/1995'
+    ]
+];
--- database/seeds/StudentSeeder.php
+++ database/seeds/StudentSeeder.php
@@ -13,23 +13,7 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        $students = [
-            [
-                'nome' => 'Paolo',
-                'citta' => 'Roma',
-                'data_nascita' => '02/05/1998'
-            ],
-            [
-                'nome' => 'Giacomo',
-                'citta' => 'Milano',
-                'data_nascita' => '13/07/1987'
-            ],
-            [
-                'nome' => 'Aldo',
-                'citta' => 'Palermo',
-                'data_nascita' => '27/11/1995'
-            ]
-        ];
+        $students = config('students');
 
         foreach($students as $student){
             $newStudent = new Student();
